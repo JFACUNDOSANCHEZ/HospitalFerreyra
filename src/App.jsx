8a853c30ec008b735ed06e553ac6b846c459712c
Facu Sanchez <facusanchz96@gmail.com>
2025-05-06 14:13:32 -0300

    pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { } from "react";
+import { motion } from "framer-motion";
 import styles from "./App.module.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import imgAprs from "./assets/apross.jpeg"
 
 
 function App() {
-  const [hoveredRight, setHoveredRight] = useState(false);
-  const [hoveredLeft, setHoveredLeft] = useState(false);
-  const [hoveredCenter, setHoveredCenter] = useState(false);
+  
 
 
 
@@ -17,20 +15,18 @@ function App() {
       className={styles.container}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
+      transition={{ duration: 0.3 }}
     >
 
   
   <div className={styles.emergencyBanner}>
-  <a href="tel:08107772985" className={styles.emergencyButton}>
-    <i className="fas fa-phone"></i> EMERGENCIAS: 0810 XXX XXX
-  </a>
+
 </div>
       <motion.nav
         className={styles.nav}
         initial={{ x: -100 }}
         animate={{ x: 0 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.2 }}
       >
 
       </motion.nav>
@@ -39,11 +35,11 @@ function App() {
         className={styles.logoSection}
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1, delay: 0.2 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
       >
         <img
           className={styles.logo}
-          src="https://cofradiadelamerced.org.ar/wp-content/uploads/apross.jpg"
+          src={imgAprs}
           alt="Logo Apross"
         />
         <div>
@@ -58,163 +54,100 @@ function App() {
         </div>
       </motion.section>
 
-      <motion.section
-        className={styles.hero}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 0.5 }}
-      >
-        <img
-          className={styles.heroImage}
-          src="https://images.pexels.com/photos/16204310/pexels-photo-16204310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Imagen de fondo"
-        />
+  
 
+<section className={styles.heroContainer}>
+  <div
+    className={styles.heroOverlay}
+
+  >
+    <h2 className={styles.entrega}>Turnos</h2>
+    
+    <p>
+      Gestiona fácilmente tus turnos.
+      <br />
+       Sólo con un click aquí.
+    </p>
+
+
+        <motion.a
+              href="https://wa.me/5493515519207?text=Hola%2C%20quiero%20pedir%20un%20turno"
+              target="_blank"
+              rel="noopener noreferrer"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10, transition: { duration: 1 } }}
+              transition={{ duration: 1 }}
+            >
+              <br />
+              <button className={styles.ctaButton}>
+                Pedir turnos →
+              </button>
+            </motion.a>
+  </div>
+
+  <div
+    className={styles.heroOverlay}
+
+  >
+    <h2 className={styles.entrega}>Especialidades</h2>
+    <ul>
+      <li>Ecografía</li>
+   
+      <li>Cardiología</li>
+    
+      <li>Neurología</li>
       
-        <div
-          className={styles.heroOverlay}
-          style={{ left: "10%", right: "auto" }}
-          onMouseEnter={() => setHoveredLeft(true)}
-          onMouseLeave={() => setHoveredLeft(false)}
-        >
-          <h2 className={styles.entrega}>Turnos<br /></h2>
-          <p>
-            Gestiona fácilmente tus turnos con nuestro personal capacitado.Sólo con un click aqui.
-          </p>
+      <li>Dermatología</li>
+      <li>Ginecología</li>
+    </ul>
+  </div>
 
-          <AnimatePresence>
-            {hoveredLeft && (
-              <motion.a
-                href="https://equisdiagnostico.com.ar/"
-                target="_blank"
-                rel="noopener noreferrer"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10, transition: { duration: 1 } }}
-                transition={{ duration: 1 }}
-              >
-                <br />
-                <button className={styles.ctaButton}>
-                  Pedir turnos →
-                </button>
-              </motion.a>
-            )}
-          </AnimatePresence>
-        </div>
+  <div
+    className={styles.heroOverlay}
 
+  >
+    <h2 className={styles.entrega}>Entrega digital</h2>
 
-
+    <p>
+      Accede al informe de tus estudios sin moverte.
       
+       Sólo con un click aquí.
+    </p>
 
+ 
+          <motion.a
+                         href="https://hferreyra.informemedico.com.ar/pacientes"
+                         target="_blank"
+                         rel="noopener noreferrer"
+                         initial={{ opacity: 0, y: 10 }}
+                         animate={{ opacity: 1, y: 0 }}
+                         exit={{ opacity: 0, y: 10, transition: { duration: 1 } }}
+                         transition={{ duration: 1 }}
+                       >
+                         <br />
+                         <button className={styles.ctaButton}>
+                         Ver estudios →
+                         </button>
+                       </motion.a>
+  </div>
+</section>
 
-<div
-  className={styles.heroOverlay}
-  style={{ left: "50%", transform: "translateX(-50%)" }}
-  onMouseEnter={() => setHoveredCenter(true)}
-  onMouseLeave={() => setHoveredCenter(false)}
->
-  <h2 className={styles.entrega}>Especialidades</h2>
-  <ul>
-    <li>Ecografía</li>
-    <li>Cardiología</li>
-    <li>Neurología</li>
-    <li>Dermatología</li>
-    <li>Ginecología</li>
-  </ul>
-
-  <AnimatePresence>
-            {hoveredCenter && (
-              <motion.a
-                href="https://equisdiagnostico.com.ar/"
-                target="_blank"
-                rel="noopener noreferrer"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10, transition: { duration: 1 } }}
-                transition={{ duration: 1 }}
-              >
-                <br />
-                <button className={styles.ctaButton}>
-                  Descargar estudios →
-                </button>
-              </motion.a>
-            )}
-          </AnimatePresence>
-</div>
-
-
-      
-        <div
-          className={styles.heroOverlay}
-          style={{ right: "10%", left: "auto" }}
-          onMouseEnter={() => setHoveredRight(true)}
-          onMouseLeave={() => setHoveredRight(false)}
-        >
-          <h2 className={styles.entrega}>Entrega digital<br /></h2>
-          <p>
-       Accede al informe de sus estudios sin necesidad de asistir
-            personalmente a nuestro centro. Sólo con un click aqui.
-          </p>
-
-          <AnimatePresence>
-            {hoveredRight && (
-              <motion.a
-                href="https://equisdiagnostico.com.ar/"
-                target="_blank"
-                rel="noopener noreferrer"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10, transition: { duration: 0.5 } }}
-                transition={{ duration: 0.5 }}
-              >
-                <br />
-                <button className={styles.ctaButton}>
-                  Descargar estudios →
-                </button>
-              </motion.a>
-            )}
-          </AnimatePresence>
-        </div>
-
-      
         
      
-      </motion.section>
+ 
 
       <motion.footer
   className={styles.footer}
   initial={{ opacity: 0 }}
   animate={{ opacity: 1 }}
-  transition={{ duration: 1, delay: 1.5 }}
+  transition={{ duration: 0.5, delay: 0.2 }}
 >
-  <p>© 2025 Apross. Córdoba, Argentina.</p>
+  <p>© 2025 TRS24. Córdoba, Argentina.</p>
 
   
   <div className={styles.socialLinks}>
-    <a
-      href="https://wa.me/5493516089800"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="WhatsApp"
-    >
-      <i className="fab fa-whatsapp"></i>
-    </a>
-    <a
-      href="https://www.instagram.com/"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Instagram"
-    >
-      <i className="fab fa-instagram"></i>
-    </a>
-    <a
-      href="https://www.facebook.com/"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Facebook"
-    >
-      <i className="fab fa-facebook-f"></i>
-    </a>
+ 
   </div>
 </motion.footer>
 
